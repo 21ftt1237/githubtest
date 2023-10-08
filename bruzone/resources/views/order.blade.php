9a286c8994e69a91a97aa0813aeaf17d0eebacfb
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-08 19:44:39 +0800

    Update order.blade.php

--- bruzone/resources/views/order.blade.php
+++ bruzone/resources/views/order.blade.php
@@ -5,6 +5,49 @@
         </h2>
     </x-slot>
 
+    <main>
+            <h1>Order List</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Status</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Order Date & Time</th>
+                        <th>Delivery Time</th>
+                        <th>Delivery Location</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Complete</td>
+                        <td>Item 1</td>
+                        <td>$10.99</td>
+                        <td>2023-10-06 09:30 AM</td>
+                        <td>2:00 PM</td>
+                        <td>123 Main St, City</td>
+                    </tr>
+                    <tr>
+                        <td>Incomplete</td>
+                        <td>Item 2</td>
+                        <td>$19.99</td>
+                        <td>2023-10-06 11:45 AM</td>
+                        <td>Not scheduled</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>Complete</td>
+                        <td>Item 3</td>
+                        <td>$15.49</td>
+                        <td>2023-10-06 01:15 PM</td>
+                        <td>3:30 PM</td>
+                        <td>456 Elm St, Town</td>
+                    </tr>
+                    <!-- Add more rows as needed -->
+                </tbody>
+            </table>
+        </main>
+
 
         </div>
     </div>
