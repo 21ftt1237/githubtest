b93c4b208212c3533a1dd6de9b70dce18a37e73f
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-09 00:06:33 +0800

    Update navigation.blade.php

--- bruzone/resources/views/layouts/navigation.blade.php
+++ bruzone/resources/views/layouts/navigation.blade.php
@@ -15,14 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('order')" :active="request()->routeIs('order')">
-                     Order @if ($incompleteOrderCount > 0)
-                    <span class="bg-red-500 text-white px-2 py-1 rounded-full text-xs ml-1">
-                    {{ $incompleteOrderCount }}
-                     </span>
-                      @endif
-                   </x-nav-link>
-
+                  <span>{{ $incompleteOrderCount }}</span>
                 </div>
             </div>
 
