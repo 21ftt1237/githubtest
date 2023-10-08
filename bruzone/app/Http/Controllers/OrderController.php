afa2274289beb03373393641964be811fd962abb
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-08 19:39:49 +0800

    Update OrderController.php

--- bruzone/app/Http/Controllers/OrderController.php
+++ bruzone/app/Http/Controllers/OrderController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        // Your code for handling the "order" page goes here
+        return view('order'); // This assumes you have a "order.blade.php" view file
+    }
 }
