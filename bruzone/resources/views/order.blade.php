168502f6b42f33ad7d8182f260f2e4ed43f37c13
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-08 19:47:17 +0800

    Update order.blade.php

--- bruzone/resources/views/order.blade.php
+++ bruzone/resources/views/order.blade.php
@@ -51,3 +51,69 @@
         </div>
     </div>
 </x-app-layout>
+
+   <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            border: 1px solid #ccc;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        /* Updated CSS for the logo placement */
+        .logo {
+            position: absolute;
+            top: 0;
+            left: 0;
+            margin: 20px; /* Adjust margin as needed */
+        }
+        .notification-badge {
+            background-color: red;
+            color: white;
+            border-radius: 50%;
+            padding: 4px 8px;
+            position: absolute;
+            top: 0;
+            right: 0;
+            font-size: 14px;
+        }
+        /* Updated CSS for the navigation menu/bar */
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover:not(.active) {
+            background-color: #111;
+        }
+
+        .active {
+            background-color: #04AA6D;
+        }
+    </style>
