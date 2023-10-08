f554a442f27c96a0e7c6e1c0a857eab3e4a5827c
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-08 19:33:42 +0800

    Update navigation.blade.php

--- bruzone/resources/views/layouts/navigation.blade.php
+++ bruzone/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('order')" :active="request()->routeIs('dashboard')">
+                        {{ __('Order') }}
+                    </x-nav-link>
                 </div>
             </div>
 
