d245c1501ef2b9a42c0ad1ac8bef12ebb4d89e27
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-11 13:35:50 +0800

    Update OrderController.php

--- bruzone/app/Http/Controllers/OrderController.php
+++ bruzone/app/Http/Controllers/OrderController.php
@@ -5,21 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use DateTime; // Import DateTime class
+use App\Order; // Import the Order model
 
 class OrderController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $orders = DB::table('orders')->get();
-
         $incompleteOrderCount = DB::table('orders')->where('delivery_status', 'incomplete')->count();
 
         foreach ($orders as $order) {
             // Convert ordered_datetime to a DateTime object
             $order->ordered_datetime = new DateTime($order->ordered_datetime);
-            $order->delivery_time = new DateTime($order->delivery_time);
+            if ($order->delivery_time != null) {
+                $order->delivery_time = new DateTime($order->delivery_time);
+            } else {
+                $order->delivery_time = null; // Set it to null for cases with no scheduled time.
+            }
         }
 
-        return view('order', compact('orders', 'incompleteOrderCount'));
+        return view('order.index', compact('orders', 'incompleteOrderCount'));
+    }
+
+    public function track(Request $request)
+    {
+        $order_id = $request->input('order_id');
+        
+        $order = Order::find($order_id);
+        
+        return view('order_status', compact('order'));
     }
 }
