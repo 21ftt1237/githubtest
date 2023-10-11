5ad48bb4a020e53455bce5ae128d52ed3bafba83
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-11 22:53:42 +0800

    Update order_status.blade.php

--- bruzone/resources/views/order_status.blade.php
+++ bruzone/resources/views/order_status.blade.php
@@ -41,15 +41,6 @@
                                 <td>{{ $order->ordered_datetime->format('Y-m-d h:i A') }}</td>
                                 <td>{{ $order->delivery_time != null ? $order->delivery_time->format('h:i A') : 'Not scheduled' }}</td>
                                 <td>{{ $order->delivery_location }}</td>
-                                <td class="track-order-link">
-                                   <form method="post" action="{{ route('order_status') }}">
-                                  @csrf
-                             <input type="hidden" name="order_id" value="{{ $order->id }}">
-                               <button type="submit">
-                              {{ __('Track Order') }}
-                                </button>
-                                </form>
-                                </td>
                             </tr>
                             @endforeach
                         </tbody>
@@ -60,4 +51,49 @@
     </div>
 </x-app-layout>
 
-</x-app-layout>
+ <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            background-color: white;
+        }
+        th, td {
+            border: 1px solid #ccc;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        /* Updated CSS for the logo placement */
+        .logo {
+            position: absolute;
+            top: 0;
+            left: 0;
+            margin: 20px; /* Adjust margin as needed */
+        }
+        .notification-badge {
+            background-color: red;
+            color: white;
+            border-radius: 50%;
+            padding: 4px 8px;
+            position: absolute;
+            top: 0;
+            right: 0;
+            font-size: 14px;
+        }
+       .button {
+       text-color:blue;
+       }
+       @media screen and (max-width: 768px) {
+    /* Define styles for mobile screens */
+    .track-order-link {
+        display: block; /* or any other suitable style */
+    }
+}
