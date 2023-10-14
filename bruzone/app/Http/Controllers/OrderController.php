1e1415a7cd44f57b321b0d03f54b1f93e569eb8b
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-14 20:37:34 +0800

    Update OrderController.php

--- bruzone/app/Http/Controllers/OrderController.php
+++ bruzone/app/Http/Controllers/OrderController.php
@@ -1,18 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use DateTime;
-
 class OrderController extends Controller
 {
     public function index(Request $request)
     {
         $orders = DB::table('orders')->get();
         $incompleteOrderCount = DB::table('orders')->where('delivery_status', 'incomplete')->count();
-
         foreach ($orders as $order) {
             $order->ordered_datetime = new DateTime($order->ordered_datetime);
             if ($order->delivery_time != null) {
@@ -21,18 +17,4 @@ class OrderController extends Controller
                 $order->delivery_time = null;
             }
         }
-
-        return view('order', compact('orders', 'incompleteOrderCount'));
-    }
-
-    public function showStatus($user_id, $order_date)
-{
-    // Fetch the list of items for the specified user ID and order date
-    $items = OrderItem::where('user_id', $user_id)
-        ->where('order_date', $order_date)
-        ->get();
-
-    // Return the view with the list of items
-    return view('order_status', ['items' => $items]);
-}
 }
