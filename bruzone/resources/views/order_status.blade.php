aaee189833185a25d3456277463418a19ef1f26c
21ftt1237 <99056219+21ftt1237@users.noreply.github.com>
2023-10-14 16:43:24 +0800

    Update order_status.blade.php

--- bruzone/resources/views/order_status.blade.php
+++ bruzone/resources/views/order_status.blade.php
@@ -1,18 +1,7 @@
 <x-app-layout>
-
-     <x-slot name="header">
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-green-gray dark:text-gray-200 leading-tight">
             {{ __('Order Status:') }}
         </h2>
     </x-slot>
-      <div class="py-12">
-        <div class="bg-blue dark:bg-white overflow-hidden shadow-sm sm:rounded-lg">
-    <div class="min-h-screen bg-blue-200">
-    <h1>Welcome to my page!</h1>
-    </div>
-    </div>
-    </div>
-
-   
 </x-app-layout>
-
